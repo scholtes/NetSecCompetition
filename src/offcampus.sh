bd66440eab628daa4a1b8d9aeda2f17e2380a0e3
scholtes <scholtes.garrett@gmail.com>
2017-03-10 15:19:30 -0500

    wrong domain name

--- src/offcampus.sh
+++ src/offcampus.sh
@@ -1,2 +1,2 @@
-xterm -e ssh -L 8180:helios.ececs.uc.edu:8180 scholtgt@filespace.uc.edu & 
-xterm -e ssh -R 20423:localhost:20423 scholtgt@filespace.uc.edu &
+xterm -e ssh -L 8180:helios.ececs.uc.edu:8180 scholtgt@ucfilespace.uc.edu & 
+xterm -e ssh -R 20423:localhost:20423 scholtgt@ucfilespace.uc.edu &
